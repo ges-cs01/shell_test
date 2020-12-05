bcd12c3eddf40a1480a128a5370ca896c933eb64
ges-cs01 <54139472+ges-cs01@users.noreply.github.com>
2020-12-05 11:26:32 -0300

    Update and rename hello..sh to loops.sh

--- hello..sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for x in {a..z}; do for i in {1..20}; do echo var $x$i = document.getElementById\(\"${x^}$i\"\); done; done
---
+++ loops.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for x in {a..z}; 
+do 
+  for i in {1..20}; 
+  do echo var $x$i = document.getElementById\(\"${x^}$i\"\); 
+  done; 
+done
